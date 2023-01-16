94d1c9cbdbb50e56c989a1c6e32078ed159b7d48
Hakan Hardal <>
2023-01-17 01:11:50 +0300

    first commit bunu yaptım

---
+++ gitproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gitproject
+//
+//  Created by Hakan Hardal on 17.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Git")
+    }
+
+
+}
+
